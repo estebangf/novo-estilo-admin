936d3cc8139de4265469246ac2b81d02eb59e65c
estebangf <esteban.fernandez93@outlook.com.ar>
2024-10-09 13:31:48 -0300

    Modificación de lectura de notificaciones para disminuir la carga de lectura de la db

--- src/Components/AppBarStyled/AppBarStyled.tsx
+++ src/Components/AppBarStyled/AppBarStyled.tsx
@@ -1,24 +1,34 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { useEffect, useState } from 'react';
-import HomeIcon from '@mui/icons-material/Home';
-import { useLocation } from 'react-router-dom';
-import AuthStatus from '../../Pages/Auth/AuthStatus';
-import { useApp, useAuth, useScrollPosition } from '../../Tools/Hooks';
-import LinkStyled from '../LinkStyled';
-import { BREADCRUMBS } from '../../Models/Breadcrumb';
-import { Badge, Divider, Menu, MenuItem } from '@mui/material';
-import NotificationItem from '../NotificationItem';
-import NotificationModel from '../../Models/Notifications';
-import { notificationConverter, NOTIFICATIONS_COLLECTION } from '../../Models/Notifications';
-import { onSnapshot, query, collection, orderBy } from 'firebase/firestore';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useEffect, useRef, useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import { useLocation } from "react-router-dom";
+import AuthStatus from "../../Pages/Auth/AuthStatus";
+import { useApp, useAuth, useScrollPosition } from "../../Tools/Hooks";
+import LinkStyled from "../LinkStyled";
+import { BREADCRUMBS } from "../../Models/Breadcrumb";
+import { Badge, Divider, Menu, MenuItem } from "@mui/material";
+import NotificationItem from "../NotificationItem";
+import NotificationModel from "../../Models/Notifications";
+import {
+  notificationConverter,
+  NOTIFICATIONS_COLLECTION,
+} from "../../Models/Notifications";
+import {
+  onSnapshot,
+  query,
+  collection,
+  orderBy,
+  limit,
+  Unsubscribe,
+} from "firebase/firestore";
 
 const onTopTheme = createTheme({
   // palette: {
@@ -37,7 +47,8 @@ const onTopTheme = createTheme({
           padding: 4,
           margin: 12,
           zIndex: 1100,
-          transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+          transition:
+            "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
           // transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
           boxShadow: "none",
           // boxShadow: "rgb(0 0 0 / 3%) 0rem 0.25rem 0.6875rem 0rem",
@@ -45,12 +56,11 @@ const onTopTheme = createTheme({
           color: "rgb(123, 128, 154)",
           backgroundColor: "transparent",
           // backgroundColor: "#ffffff26"
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
-
 
 const scrollingTheme = createTheme({
   components: {
@@ -63,41 +73,45 @@ const scrollingTheme = createTheme({
           padding: 4,
           margin: 12,
           zIndex: 1100,
-          transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+          transition:
+            "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
           // transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
           boxSizing: "border-box",
           boxShadow: "rgb(0 0 0 / 5%) 0rem 1.25rem 1.6875rem 0rem",
           backdropFilter: "saturate(200%) blur(1.875rem)",
           backgroundColor: "rgba(255, 255, 255, 0.8)",
           color: "rgb(52, 71, 103)",
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
 
 interface AppBarStyledProps {
-  setOpenDrawerMovile: Function,
-  setOpenDrawerDesktop: Function,
-  openDrawerDesktop: boolean
+  setOpenDrawerMovile: Function;
+  setOpenDrawerDesktop: Function;
+  openDrawerDesktop: boolean;
 }
-function AppBarStyled({ setOpenDrawerMovile, setOpenDrawerDesktop, openDrawerDesktop }: AppBarStyledProps) {
-  const app = useApp()
-  const auth = useAuth()
-  const loc = useLocation()
+function AppBarStyled({
+  setOpenDrawerMovile,
+  setOpenDrawerDesktop,
+  openDrawerDesktop,
+}: AppBarStyledProps) {
+  const app = useApp();
+  const auth = useAuth();
+  const loc = useLocation();
 
   const [scrolling, setScrolling] = useState(false);
   const scrollTop = 60;
   const scrollPosition = useScrollPosition();
 
   useEffect(() => {
-    setScrolling(scrollPosition > scrollTop)
+    setScrolling(scrollPosition > scrollTop);
     // console.log(scrolling, " ", scrollPosition)
   }, [scrollPosition]);
 
-
-
   const [notifications, setNotifications] = useState<NotificationModel[]>([]);
+  const notificationsSubscribe = useRef<Unsubscribe>();
   const [visiblesNotifications, setVisiblesNotifications] = useState(5);
   const [unReaded, setUnReaded] = useState<number>(0);
 
@@ -105,53 +119,60 @@ function AppBarStyled({ setOpenDrawerMovile, setOpenDrawerDesktop, openDrawerDes
   const openNoty = Boolean(anchorElNoty);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNoty(event.currentTarget)
+    setAnchorElNoty(event.currentTarget);
   };
   const handleCloseNotifications = () => {
-    setAnchorElNoty(null)
-    setVisiblesNotifications(5)
+    setAnchorElNoty(null);
+    setVisiblesNotifications(5);
   };
 
-
-
   useEffect(() => {
+    if (notificationsSubscribe.current) notificationsSubscribe.current();
     // const q = query(collection(db, TURNS_COLLECTION), where("date", ">", "CA"));
-    const unsubscribe = onSnapshot(query(
-      collection(app.firestore, NOTIFICATIONS_COLLECTION),
-      orderBy("date", "desc")
-    ).withConverter(notificationConverter), (querySnapshot) => {
-      let notificationsSnapshot: NotificationModel[] = [];
+    notificationsSubscribe.current = onSnapshot(
+      query(
+        collection(app.firestore, NOTIFICATIONS_COLLECTION),
+        orderBy("date", "desc"),
+        limit(visiblesNotifications)
+      ).withConverter(notificationConverter),
+      (querySnapshot) => {
+        let notificationsSnapshot: NotificationModel[] = [];
 
-      // querySnapshot.docChanges().forEach(change => {
-      //   let changed: NotificationModel = change.doc.data()
-      //   console.log("Changed: ", changed)
-      //   if (change.type === 'added' && !changed.seedForUsers.includes(auth.user!.uid)) {
-      //     var title = changed.title;
-      //     var icon = 'https://novo-estilo-app.web.app/logo192.png';
-      //     var body = changed.description;
-      //     var notification = new Notification(title, { body, icon });
-      //   }
-      //   // if (change.type === 'modified') {
-      //   //   console.log('Modified city: ', change.doc.data());
-      //   // }
-      //   // if (change.type === 'removed') {
-      //   //   console.log('Removed city: ', change.doc.data());
-      //   // }
-      // });
+        // querySnapshot.docChanges().forEach(change => {
+        //   let changed: NotificationModel = change.doc.data()
+        //   console.log("Changed: ", changed)
+        //   if (change.type === 'added' && !changed.seedForUsers.includes(auth.user!.uid)) {
+        //     var title = changed.title;
+        //     var icon = 'https://novo-estilo-app.web.app/logo192.png';
+        //     var body = changed.description;
+        //     var notification = new Notification(title, { body, icon });
+        //   }
+        //   // if (change.type === 'modified') {
+        //   //   console.log('Modified city: ', change.doc.data());
+        //   // }
+        //   // if (change.type === 'removed') {
+        //   //   console.log('Removed city: ', change.doc.data());
+        //   // }
+        // });
 
-      querySnapshot.forEach((notification) => {
-        notificationsSnapshot.push(notification.data());
-      });
-      setNotifications(notificationsSnapshot)
-    });
-  }, [])
+        querySnapshot.forEach((notification) => {
+          notificationsSnapshot.push(notification.data());
+        });
+        setNotifications(notificationsSnapshot);
+      }
+    );
+
+    return () => {
+      if (notificationsSubscribe.current) notificationsSubscribe.current();
+    };
+  }, [app.firestore, visiblesNotifications]);
 
   useEffect(() => {
-    let _unReaded = notifications.filter(_notification => {
-      return !_notification.seedForUsers.includes(auth.user!.uid)
-    }).length
-    setUnReaded(_unReaded)
-  }, [notifications])
+    let _unReaded = notifications.filter((_notification) => {
+      return !_notification.seedForUsers.includes(auth.user!.uid);
+    }).length;
+    setUnReaded(_unReaded);
+  }, [notifications]);
 
   return (
     <ThemeProvider theme={scrolling ? scrollingTheme : onTopTheme}>
@@ -159,42 +180,58 @@ function AppBarStyled({ setOpenDrawerMovile, setOpenDrawerDesktop, openDrawerDes
         <Toolbar>
           <IconButton
             sx={{
-              display: { xs: 'inline-flex', sm: 'none' },
+              display: { xs: "inline-flex", sm: "none" },
             }}
-            onClick={() => setOpenDrawerMovile(true)} edge="start" color="inherit" aria-label="menu">
+            onClick={() => setOpenDrawerMovile(true)}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <IconButton
             sx={{
-              display: { xs: 'none', sm: 'inline-flex' },
+              display: { xs: "none", sm: "inline-flex" },
               marginLeft: -2,
-              marginRight: 2
+              marginRight: 2,
             }}
-            onClick={() => setOpenDrawerDesktop(!openDrawerDesktop)} edge="start" color="inherit" aria-label="menu">
+            onClick={() => setOpenDrawerDesktop(!openDrawerDesktop)}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
             {openDrawerDesktop ? <MenuOpenIcon /> : <MenuIcon />}
           </IconButton>
           <div style={{ flexGrow: 1 }}>
-            {loc.pathname != "/" &&
+            {loc.pathname != "/" && (
               <Breadcrumbs separator="›" aria-label="breadcrumb">
                 <LinkStyled
-                  sx={{ display: 'flex', alignItems: 'center' }}
+                  sx={{ display: "flex", alignItems: "center" }}
                   color="inherit"
                   to="/"
                 >
                   <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
                 </LinkStyled>
-                {loc.pathname.split("/").filter(x => BREADCRUMBS[x]).map(_pathname =>
-                  <LinkStyled
-                    color="inherit"
-                    to={`${loc.pathname.split(_pathname)[0]}${_pathname}`}
-                  >
-                    {BREADCRUMBS[_pathname]}
-                  </LinkStyled>
-                )}
+                {loc.pathname
+                  .split("/")
+                  .filter((x) => BREADCRUMBS[x])
+                  .map((_pathname) => (
+                    <LinkStyled
+                      color="inherit"
+                      to={`${loc.pathname.split(_pathname)[0]}${_pathname}`}
+                    >
+                      {BREADCRUMBS[_pathname]}
+                    </LinkStyled>
+                  ))}
               </Breadcrumbs>
-            }
+            )}
 
-            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
               {app.title}
             </Typography>
           </div>
@@ -206,17 +243,17 @@ function AppBarStyled({ setOpenDrawerMovile, setOpenDrawerDesktop, openDrawerDes
             color="inherit"
             id="NotificationIcon"
           >
-            {unReaded > 0 ?
+            {unReaded > 0 ? (
               <Badge badgeContent={unReaded} color="error">
                 <NotificationsIcon />
-              </Badge> :
+              </Badge>
+            ) : (
               <NotificationsIcon color="disabled" />
-            }
+            )}
           </IconButton>
           <AuthStatus />
         </Toolbar>
       </AppBar>
-
 
       <Menu
         anchorEl={anchorElNoty}
@@ -235,15 +272,22 @@ function AppBarStyled({ setOpenDrawerMovile, setOpenDrawerDesktop, openDrawerDes
           },
         }}
       >
-        {notifications.slice(0, visiblesNotifications).map((notification, indexN) => {
-          return (
-            <NotificationItem handleClick={handleCloseNotifications} notification={notification} />
-          )
-        })}
+        {notifications
+          .slice(0, visiblesNotifications)
+          .map((notification, indexN) => {
+            return (
+              <NotificationItem
+                handleClick={handleCloseNotifications}
+                notification={notification}
+              />
+            );
+          })}
         {notifications.length === 0 ? (
           <MenuItem>No tienes notificaciones.</MenuItem>
         ) : notifications.length - 1 > visiblesNotifications ? (
-          <MenuItem onClick={() => setVisiblesNotifications(p => p + 5)}>Mostrar mas ...</MenuItem>
+          <MenuItem onClick={() => setVisiblesNotifications((p) => p + 5)}>
+            Mostrar mas ...
+          </MenuItem>
         ) : (
           <MenuItem>Fin de la lista</MenuItem>
         )}
--- src/Pages/Turns/TurnsList.tsx
+++ src/Pages/Turns/TurnsList.tsx
@@ -1,43 +1,70 @@
-import { Add, ExpandLess, ExpandMore, Inbox } from "@mui/icons-material";
-import { Box, Button, Collapse, Dialog, DialogActions, DialogTitle, Fab, FormControlLabel, FormGroup, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Paper, Switch, TextField, Zoom } from "@mui/material";
-import { addDoc, collection, doc, onSnapshot, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { Add, ExpandLess, ExpandMore } from "@mui/icons-material";
+import {
+  Box,
+  Collapse,
+  Fab,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  List,
+  ListSubheader,
+  Paper,
+  Switch,
+  TextField,
+  Zoom,
+} from "@mui/material";
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import DialogTurn from "../../Components/DialogTurn/DialogTurn";
 import TurnItem from "../../Components/TurnItem";
 import TurnsImage from "../../Components/TurnsImage";
-import Turn, { getDate, getFullDate, getNewDateWithNewTime, turnConverter, TurnsListExtra, TURNS_COLLECTION } from "../../Models/Turn";
-import { Works } from "../../Models/Work";
+import Turn, {
+  getDate,
+  turnConverter,
+  TurnsListExtra,
+  TURNS_COLLECTION,
+} from "../../Models/Turn";
 import { getDateValue } from "../../Tools";
 import { useApp } from "../../Tools/Hooks";
-
 
 function TurnsList() {
   const app = useApp();
 
-  const [desde, setDesde] = useState<Date>(new Date())
+  const [desde, setDesde] = useState<Date>(new Date());
   const [hasta, setHasta] = useState<Date>(
-    new Date((new Date()).setDate((new Date()).getDate() + 7))
-  )
-  const [dateView, setDateView] = useState<number>()
-  const [viewReserveds, setViewReserveds] = useState(true)
-  const [viewOpeneds, setViewOpeneds] = useState(true)
-  const [turns, setTurns] = useState<Turn[]>([])
-  const [turnsList, setTurnsList] = useState<TurnsListExtra>([])
-  const [dialogTurn, setDialogTurn] = useState<Turn>()
+    new Date(new Date().setDate(new Date().getDate() + 7))
+  );
+  const [dateView, setDateView] = useState<number>();
+  const [viewReserveds, setViewReserveds] = useState(true);
+  const [viewOpeneds, setViewOpeneds] = useState(true);
+  const [turns, setTurns] = useState<Turn[]>([]);
+  const [turnsList, setTurnsList] = useState<TurnsListExtra>([]);
+  const [dialogTurn, setDialogTurn] = useState<Turn>();
 
   useEffect(() => {
     // const q = query(collection(db, TURNS_COLLECTION), where("date", ">", "CA"));
-    const unsubscribe = onSnapshot(query(
-      collection(app.firestore, TURNS_COLLECTION),
-      orderBy("date")
-    ).withConverter(turnConverter), (querySnapshot) => {
-      let turnsSnapshot: Turn[] = [];
-      querySnapshot.forEach((turn) => {
-        turnsSnapshot.push(turn.data());
-      });
-      setTurns(turnsSnapshot)
-    });
-  }, [])
+    const unsubscribe = onSnapshot(
+      query(
+        collection(app.firestore, TURNS_COLLECTION),
+        orderBy("date")
+      ).withConverter(turnConverter),
+      (querySnapshot) => {
+        let turnsSnapshot: Turn[] = [];
+        querySnapshot.forEach((turn) => {
+          turnsSnapshot.push(turn.data());
+        });
+        setTurns(turnsSnapshot);
+      }
+    );
+  }, []);
 
   const filterTurns = () => {
     let inicio = new Date(desde);
@@ -46,77 +73,94 @@ function TurnsList() {
     fin.setHours(23, 59, 59, 999);
 
     return turns
-      .filter(t => t.date > inicio)
-      .filter(t => t.date < fin)
-      .filter(t => viewReserveds || !t.reservedBy)
-      .filter(t => viewOpeneds || !!t.reservedBy)
-  }
+      .filter((t) => t.date > inicio)
+      .filter((t) => t.date < fin)
+      .filter((t) => viewReserveds || !t.reservedBy)
+      .filter((t) => viewOpeneds || !!t.reservedBy);
+  };
 
   useEffect(() => {
     let dates: TurnsListExtra = [];
-    filterTurns().map(t => {
-      let index = dates.findIndex(e => e.date == getDate(t))
-      if (index != -1)
-        dates[index].turns.push(t)
+    filterTurns().map((t) => {
+      let index = dates.findIndex((e) => e.date == getDate(t));
+      if (index != -1) dates[index].turns.push(t);
       else
         dates.push({
           date: getDate(t),
-          turns: [t]
-        })
-    })
+          turns: [t],
+        });
+    });
 
-    setTurnsList([...dates])
-    setDateView(undefined)
-  }, [turns, desde, hasta, viewReserveds, viewOpeneds])
+    setTurnsList([...dates]);
+    setDateView(undefined);
+  }, [turns, desde, hasta, viewReserveds, viewOpeneds]);
 
   const handleChangeView = (index: number) => {
-    setDateView(prev => prev === index ? undefined : index)
-  }
+    setDateView((prev) => (prev === index ? undefined : index));
+  };
 
   function setDesdeHandle(value: string) {
     let dateExtra = new Date(value);
-    dateExtra.setMinutes(dateExtra.getMinutes() + dateExtra.getTimezoneOffset());
+    dateExtra.setMinutes(
+      dateExtra.getMinutes() + dateExtra.getTimezoneOffset()
+    );
 
-    setDesde(dateExtra)
+    setDesde(dateExtra);
   }
   function setHastaHandle(value: string) {
     let dateExtra = new Date(value);
-    dateExtra.setMinutes(dateExtra.getMinutes() + dateExtra.getTimezoneOffset());
+    dateExtra.setMinutes(
+      dateExtra.getMinutes() + dateExtra.getTimezoneOffset()
+    );
 
-    setHasta(dateExtra)
+    setHasta(dateExtra);
   }
-
 
   function handleSaveTurn(): void {
     if (!dialogTurn) return;
 
     if (dialogTurn.id)
-      updateDoc(doc(app.firestore, TURNS_COLLECTION, dialogTurn.id).withConverter(turnConverter), dialogTurn).then(r => {
-        setDialogTurn(undefined)
-      }).catch(e => {
-        console.error("error new turn: ", e)
-      })
+      updateDoc(
+        doc(app.firestore, TURNS_COLLECTION, dialogTurn.id).withConverter(
+          turnConverter
+        ),
+        dialogTurn
+      )
+        .then((r) => {
+          setDialogTurn(undefined);
+        })
+        .catch((e) => {
+          console.error("error new turn: ", e);
+        });
     else
-      addDoc(collection(app.firestore, TURNS_COLLECTION).withConverter(turnConverter), dialogTurn).then(r => {
-        setDialogTurn(undefined)
-      }).catch(e => {
-        console.error("error new turn: ", e)
-      })
+      addDoc(
+        collection(app.firestore, TURNS_COLLECTION).withConverter(
+          turnConverter
+        ),
+        dialogTurn
+      )
+        .then((r) => {
+          setDialogTurn(undefined);
+        })
+        .catch((e) => {
+          console.error("error new turn: ", e);
+        });
   }
 
-
   return (
-    <Box sx={{
-      padding: 2,
-      paddingBottom: 8
-    }}>
+    <Box
+      sx={{
+        padding: 2,
+        paddingBottom: 8,
+      }}
+    >
       <TurnsImage turns={filterTurns()} extended={!viewOpeneds} />
       <TextField
         id="date"
         label="Desde"
         type="date"
         value={getDateValue(desde)}
-        onChange={e => setDesdeHandle(e.target.value)}
+        onChange={(e) => setDesdeHandle(e.target.value)}
         sx={{ width: 250, marginBottom: 2, marginRight: 2 }}
         InputLabelProps={{
           shrink: true,
@@ -127,7 +171,7 @@ function TurnsList() {
         label="Hasta"
         type="date"
         value={getDateValue(hasta)}
-        onChange={e => setHastaHandle(e.target.value)}
+        onChange={(e) => setHastaHandle(e.target.value)}
         sx={{ width: 250, marginBottom: 2, marginRight: 2 }}
         InputLabelProps={{
           shrink: true,
@@ -135,20 +179,37 @@ function TurnsList() {
       />
       <br />
       <FormGroup>
-        <FormControlLabel control={<Switch checked={viewReserveds} onChange={e => setViewReserveds(prev => !prev)} />} label="Mostrar reservados" />
-        <FormControlLabel control={<Switch checked={viewOpeneds} onChange={e => setViewOpeneds(prev => !prev)} />} label="Mostrar los libres" />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={viewReserveds}
+              onChange={(e) => setViewReserveds((prev) => !prev)}
+            />
+          }
+          label="Mostrar reservados"
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={viewOpeneds}
+              onChange={(e) => setViewOpeneds((prev) => !prev)}
+            />
+          }
+          label="Mostrar los libres"
+        />
       </FormGroup>
       {turnsList.map((date, index) => {
-        let open = index === dateView
+        let open = index === dateView;
         return (
           <Paper
             sx={{
-              background: open ? "#fff" : "transparent"
+              background: open ? "#fff" : "transparent",
             }}
-            elevation={open ? 2 : 0} >
+            elevation={open ? 2 : 0}
+          >
             <List
               sx={{
-                background: "transparent"
+                background: "transparent",
               }}
               aria-labelledby="nested-list-subheader"
               subheader={
@@ -157,8 +218,10 @@ function TurnsList() {
                   sx={{
                     display: "flex",
                     justifyContent: "space-between",
-                    background: "transparent"
-                  }} id="nested-list-subheader">
+                    background: "transparent",
+                  }}
+                  id="nested-list-subheader"
+                >
                   {date.date}
                   <IconButton edge="end" aria-label="delete">
                     {open ? <ExpandLess /> : <ExpandMore />}
@@ -168,42 +231,49 @@ function TurnsList() {
             >
               <Collapse in={open} timeout="auto" unmountOnExit>
                 <List>
-                  {date.turns.map(turn => <TurnItem handleEdit={() => setDialogTurn(turn)} turn={turn} />)}
+                  {date.turns.map((turn) => (
+                    <TurnItem
+                      handleEdit={() => setDialogTurn(turn)}
+                      turn={turn}
+                    />
+                  ))}
                 </List>
               </Collapse>
-            </List >
+            </List>
           </Paper>
-        )
+        );
       })}
-      <Zoom
-        in={true}
-      >
+      <Zoom in={true}>
         <Fab
-          onClick={e => setDialogTurn({
-            createdAt: new Date(),
-            date: new Date(),
-            reservedBy: null,
-            works: [],
-            allowedWorks: []
-          })}
+          onClick={(e) =>
+            setDialogTurn({
+              createdAt: new Date(),
+              date: new Date(),
+              reservedBy: null,
+              works: [],
+              allowedWorks: [],
+            })
+          }
           sx={{
             position: "fixed",
             bottom: 12,
-            right: 12
+            right: 12,
           }}
-          color="primary" aria-label="add">
+          color="primary"
+          aria-label="add"
+        >
           <Add />
         </Fab>
       </Zoom>
-      {dialogTurn &&
+      {dialogTurn && (
         <DialogTurn
           turn={dialogTurn}
           handleChangeDialogTurn={setDialogTurn}
           handleSaveTurn={handleSaveTurn}
         />
-      }
-    </Box >
-  )
+      )}
+    </Box>
+  );
 }
 
 export default TurnsList;
